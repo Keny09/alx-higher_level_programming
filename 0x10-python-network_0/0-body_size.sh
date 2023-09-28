d2bf6a968f5ba5bbc7e0cf1ef9a533b2a92427be
Keny09 <rosekendy09@gmail.com>
2023-09-28 21:23:36 +0300

    python networking

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Check if the URL is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a request to the URL and get the size of the body of the response
+response_size=$(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}')
+
+# Check if the size is available
+if [ -z "$response_size" ]; then
+    echo "Failed to get the size of the response body."
+else
+    echo "The size of the response body is $response_size bytes."
+fi
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,23 @@
+​​#!/bin/bash
+
+# Check if the URL is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a DELETE request to the URL and store the response
+response=$(curl -s -w "\n%{http_code}" -X DELETE "$1")
+
+# Get the HTTP status code from the response
+status_code=$(echo "$response" | tail -n1)
+
+# Get the body of the response
+body=$(echo "$response" | sed '$d')
+
+# Display only the body of a 200 status code response
+if [ "$status_code" -eq 200 ]; then
+    echo "$body"
+else
+    echo "Request failed with status code $status_code"
+fi
