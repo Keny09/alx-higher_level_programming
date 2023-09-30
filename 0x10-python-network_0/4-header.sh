8bce9699b741985832407d97c8349158c28c71c1
Keny09 <rosekendy09@gmail.com>
2023-09-30 10:58:51 +0300

    python networking

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Check if the URL is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
+# Check if the URL argument is provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
 fi
 
-# Send a GET request with the specified header
-response=$(curl -s -H "X-School-User-Id: 98" "$1")
+# Set the URL and custom header value
+url="$1"
+header_value="98"
+
+# Send a GET request with the custom header using curl
+response=$(curl -s -H "X-School-User-Id: $header_value" "$url")
 
 # Display the body of the response
 echo "$response"
