04bbd2f9ce250a5426c67873fe44a16d7bfc2deb
Keny09 <rosekendy09@gmail.com>
2023-10-02 15:41:41 +0300

    python networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,29 @@
-#!/usr/bin/python3
-# Check if the URL is provided
-if [ -z "$1" ]; then
+#!/bin/bash
+
+# Check if the URL argument is provided
+if [ $# -ne 1 ]; then
   echo "Usage: $0 <URL>"
   exit 1
 fi
 
 url="$1"
 
-# Send a request to the URL and get the size of the body of the response
-response_size=$(curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}')
+# Use curl to send a GET request and save the response body to a temporary file
+response_file=$(mktemp)
+curl -s -o "$response_file" "$url"
 
-# Check if the size is available
-if [ -z "$response_size" ]; then
-    echo "Failed to get the size of the response body."
-else
-    echo "The size of the response body is $response_size bytes."
+# Check if curl encountered any errors
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to retrieve the URL: $url"
+  rm -f "$response_file"
+  exit 1
 fi
+
+# Get the size of the response body in bytes
+response_size=$(wc -c < "$response_file")
+
+# Display the size of the response body in bytes
+echo "Size of the response body: $response_size bytes"
+
+# Clean up the temporary response file
+rm -f "$response_file"
