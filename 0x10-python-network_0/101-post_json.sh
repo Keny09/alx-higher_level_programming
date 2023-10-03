27afb46f608e9e7847e486e17c44eed964b1d33b
Keny09 <rosekendy09@gmail.com>
2023-10-03 07:11:57 -0700

    python network

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Get the URL and filename from the command line arguments
-url=$1
-filename=$2
-
-# Read the contents of the file into a variable
-file_contents=$(cat $filename)
-
-# Send a POST request to the URL with the file contents in the body
-response=$(curl -s -X POST -H "Content-Type: application/json" -d "$file_contents" $url)
-
-# Display the body of the response
-echo $response
+# This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s -X POST -H "Content-Type: application/json" -d @$2 "$1"
