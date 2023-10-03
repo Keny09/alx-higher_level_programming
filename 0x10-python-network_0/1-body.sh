1df8a8d13284bd3aacbdc2d7725062a2eb1b9f81
Keny09 <rosekendy09@gmail.com>
2023-10-03 06:41:24 -0700

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL using curl, and displays the body of the response
-curl -s "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1"
