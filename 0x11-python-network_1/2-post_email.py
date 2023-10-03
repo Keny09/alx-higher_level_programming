b917631bfe01d7202e78c478f98894f3d1363deb
Keny09 <rosekendy09@gmail.com>
2023-10-03 05:53:45 +0300

    python networking

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,20 @@
+python
 import urllib.request
 import urllib.parse
 import sys
 
-# Check if a URL and email are provided as arguments
-if len(sys.argv) < 3:
-    print("Usage: python script.py <URL> <email>")
-    sys.exit(1)
+def send_post_request(url, email):
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
 
-url = sys.argv[1]
-email = sys.argv[2]
+    try:
+        with urllib.request.urlopen(url, data) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.URLError as e:
+        print(f"Error: {e.reason}")
 
-# Encode the email parameter
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-
-try:
-    # Send a POST request to the URL with the email parameter
-    with urllib.request
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    send_post_request(url, email)
