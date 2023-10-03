07f99e096f096a18e787c8e98c30e1d88b45a728
Keny09 <rosekendy09@gmail.com>
2023-10-03 10:06:23 -0700

    python network

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
-
-# Check if the URL is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send an OPTIONS request to the URL and store the response
-response=$(curl -s -w "\n%{http_code}" -X OPTIONS "$1")
-
-# Get the HTTP status code from the response
-status_code=$(echo "$response" | tail -n1)
-
-# Get the body of the response
-body=$(echo "$response" | sed '$d')
-
-# Display only the body of a 200 status code response
-if [ "$status_code" -eq 200 ]; then
-    echo "$body"
-else
-    echo "Request failed with status code $status_code"
-fi
+#A script that takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep -i "Allow" | cut -c8-
