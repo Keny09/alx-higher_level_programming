7331ca42566558e909042ca936cb5ec3d6f8c005
Keny09 <rosekendy09@gmail.com>
2023-10-03 05:08:48 +0300

    python networking

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+python
+import urllib.request
+
+try:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read().decode()
+        print(body)
+except urllib.error.URLError as e:
+    print(f"Failed to reach the server: {e.reason}")
+except Exception as e:
+    print(f"An error occurred: {e}")
