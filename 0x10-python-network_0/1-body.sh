47c54f446c830ee4afb73b85a4926c4d4ef21133
Keny09 <rosekendy09@gmail.com>
2023-10-03 06:11:20 -0700

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-# Check if the URL is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
+# This script sends a GET request to a URL and displays the body of the response (if status code is 200)
 
-# Send a GET request to the URL and store the response
-response=$(curl -s -w "\n%{http_code}" "$1")
+curl -s -o /dev/null -w "%{http_code}" "$1" | { read status_code; [ "$status_code" -eq 200 ] && curl -s "$1"; }
 
-# Get the HTTP status code from the response
-status_code=$(echo "$response" | tail -n1)
-
-# Get the body of the response
-body=$(echo "$response" | sed '$d')
-
-# Display only the body of a 200 status code response
-if [ "$status_code" -eq 200 ]; then
-    echo "$body"
-else
-    echo "Request failed with status code $status_code"
-fi
