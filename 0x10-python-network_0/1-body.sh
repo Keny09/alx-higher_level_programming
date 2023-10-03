45e19c5f928c0bce97afeacdb5713aa6cb107fd1
Keny09 <rosekendy09@gmail.com>
2023-10-03 06:35:36 -0700

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to a URL and displays the body of the response (if status code is 200)
-
-curl -s -o /dev/null -w "%{http_code}" "$1" | { read status_code; [ "$status_code" -eq 200 ] && curl -s "$1"; }
-
+# This script takes in a URL, sends a GET request to the URL using curl, and displays the body of the response
+curl -s "$1"
