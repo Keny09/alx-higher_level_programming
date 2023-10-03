98aa2a7668b61dd108bc79f4783aaf7db96b34f9
Keny09 <rosekendy09@gmail.com>
2023-10-03 10:26:33 -0700

    python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,16 @@
-def find_peak(list_of_integers):
+#!/usr/bin/python3
+"""
+contains a funct
+"""
+
+
+def find_peak(arr):
     """
-    This function finds a peak in a list of unsorted integers.
-    The function uses a divide and conquer approach to find the peak.
-    It has a time complexity of O(log(n)).
+    Function finds peak
     """
-    low = 0
-    high = len(list_of_integers) - 1
-
-    while low < high:
-        mid1 = low + (high - low) // 3
-        mid2 = high - (high - low) // 3
-
-        if list_of_integers[mid1] < list_of_integers[mid2]:
-            low = mid1 + 1
-        else:
-            high = mid2 - 1
-
-    return list_of_integers[low]
+    n = len(arr)
+    for i in range(n):
+        if (i == 0 or arr[i - 1] <= arr[i]) and \
+           (i == n - 1 or arr[i + 1] <= arr[i]):
+            return arr[i]
+    return None  # No peak found
