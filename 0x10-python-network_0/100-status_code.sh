48fa859812d7d66e179837e06c97e08e0a20b7d6
Keny09 <rosekendy09@gmail.com>
2023-10-03 07:05:59 -0700

    python network

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Get the URL from the command line arguments
-url=$1
-
-# Send a request to the URL
-response=$(curl -s -o /dev/null -w "%{http_code}" $url)
-
-# Display the status code of the response
-echo $response
+# This script sends a request to a URL passed as an argument and displays only the status code of the response
+curl -s -o /dev/null -w "%{http_code}" "$1"
