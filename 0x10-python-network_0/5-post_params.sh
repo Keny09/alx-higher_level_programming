1bc3c6ee8417e7928e7a0091fdf22d74d9b22ffb
Keny09 <rosekendy09@gmail.com>
2023-10-03 10:15:05 -0700

    python network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to the passed URL with the email and subject as parameters and displays the body of the response
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" http://localhost:5000/send_email
+curl -d "email=test@gmail.com&name=John&age=30&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
