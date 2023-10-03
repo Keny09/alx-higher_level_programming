ca0f29b2534aed55bbbcf36bd598385b9ef26d89
Keny09 <rosekendy09@gmail.com>
2023-10-03 07:48:44 -0700

    python network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# Check if the URL is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Define the URL and the data to send
-url="$1"
-data="email=test@gmail.com&subject=I will always be here for PLD"
-
-# Send a POST request and display the body of the response
-response=$(curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "$data" "$url")
-echo "$response"
+# Sends a POST request to the passed URL with the email and subject as parameters and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" http://localhost:5000/send_email
