ef2da0d7904059b723ccd7c0faf8421288b79d1e
Keny09 <rosekendy09@gmail.com>
2023-10-24 19:23:38 -0700

    web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-// prints the starwar movie title by id
 
+const urlPath = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 const request = require('request');
-const requestURL = 'http://swapi.co/api/films/' + process.argv[2];
 
-request(requestURL, (err, response, body) => {
-  if (err) throw err;
-  const dict = JSON.parse(body);
-  console.log(dict.title);
+request.get(urlPath, (error, response, body) => {
+  if (error) {
+    console.log('');
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
