c1fa1c3e8eadf1694249cd2b91d0af496412a46f
Keny09 <rosekendy09@gmail.com>
2023-10-24 19:29:38 -0700

    web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,22 @@
 #!/usr/bin/node
-// prints all charaters in starwar movie
 
+const urlPath = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 const request = require('request');
-const requestURL = 'https://swapi.co/api/films/' + process.argv[2];
 
-request(requestURL, (err, response, body) => {
-  if (err) throw err;
-  const dict = JSON.parse(body);
-  const characters = dict.characters;
-  characters.forEach(character => {
-    request(character, (error, response, data) => {
-      if (error) throw error;
-      console.log(JSON.parse(data).name);
-    });
-  });
+request.get(urlPath, (error, response, body) => {
+  if (error) {
+    console.log('');
+  } else {
+    const data = JSON.parse(body);
+    for (const urlPerson of data.characters) {
+      request.get(urlPerson, (error, response, body) => {
+        if (error) {
+          console.log('');
+        } else {
+          const person = JSON.parse(body);
+          console.log(person.name);
+        }
+      });
+    }
+  }
 });
